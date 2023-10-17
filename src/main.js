c3d5ab586af92b40b36538d1d2bce89585050066
yunwoo0301 <121915009+yunwoo0301@users.noreply.github.com>
2023-10-17 19:31:49 +0900

    JS - Header

---
+++ src/main.js
@@ -0,0 +1,16 @@
+/*window.document.querySelector();*/
+// document.querySelector('.header'); : 문자열 형태로 선택자를 넣고 해당 클래스를 찾도록 설정 
+
+const header = document.querySelector('.header'); /* header : 반환받을 요소, 문자열로 CSS 선택자 지정 */
+const headerHeight = header.offsetHeight; /*  함수를 호출해서 헤더의 높이값을 요청 */
+document.addEventListener('scroll', () => { /* addEventListener : 스크롤이 발생했을 때 일어나는 함수를 호출해줌 */
+    
+// Header에 스크롤되는 Y 좌표가 headerHeight보다 크다면 다른 스타일링!
+    console.log(window.scrollY);
+    if(window.scrollY > headerHeight) { 
+        header.classList.add('header--dark'); /* 헤더 요소의 classList에 header--dark 클래스를 추가함 */
+    } else {
+        header.classList.remove('header-dark'); /* 크기가 작다면 해당 header--dark 클래스를 제거함 */
+    }
+});
+
